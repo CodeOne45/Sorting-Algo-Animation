65342609702259dc358fb1dc59fa2706adfaab58
CodeOne45 <malikaman1999@gmail.com>
2021-07-29 23:01:43 +0200

    init conf

---
+++ sorting-alogo-anim/src/common/config.js
@@ -0,0 +1,26 @@
+import { getScreenWidth } from "./helper";
+import { BubbleSort } from "../sortFunctions/BubbleSort";
+
+// colors setting
+export const comparisionColor = "pink";
+export const swapColor = "yellow";
+export const sortedColor = "springgreen";
+export const pivotColor = "sandybrown";
+
+// time setting
+export let swapTime = 1000;
+export let compareTime = 500;
+
+// init array
+export let sortingArray = initArrayForScreenSize();
+
+export const sortingAlgorithms = [
+  { component: BubbleSort, title: "Bubble", name: "BubbleSort" },
+
+function initArrayForScreenSize() {
+  const screenSize = getScreenWidth();
+  if (screenSize < 460) return [4, 3, 2, 1];
+  else if (screenSize < 720) return [8, 7, 6, 5, 4, 3, 2, 1];
+  return [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+}
+]
